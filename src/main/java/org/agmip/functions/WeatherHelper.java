bd48b57903d56fd1bd0b2b2a9bf52edf2537d364
MengZhang <meng.zhang15@ufl.edu>
2013-06-25 11:22:36 -0400

    Add alias handling for RHMND in the getEto()

--- src/main/java/org/agmip/functions/WeatherHelper.java
+++ src/main/java/org/agmip/functions/WeatherHelper.java
@@ -253,7 +253,7 @@ public class WeatherHelper {
 
             } // Method 3 IF RHMND and RHMXD are available in the ACE data base
             else if (!(alt1 = getValueOr(dailyData, "rhmxd", "").trim()).equals("")
-                    && !(alt2 = getValueOr(dailyData, "rhmnd", "").trim()).equals("")) {
+                    && !(alt2 = getValueOr(dailyData, "rhmnd", getValueOr(dailyData, "rhumd", "")).trim()).equals("")) {
                 // ea from the given maximum and minimum Relative Humidity
                 ea = average(product(e_tMin, alt1, "0.01"), product(e_tMax, alt2, "0.01"));
 
