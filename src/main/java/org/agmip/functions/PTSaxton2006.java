57bfecf79e74d01980132a86b683fc3faec45723
MengZhang <meng.zhang15@ufl.edu>
2014-04-17 23:43:40 -0400

    Update interface structure

--- src/main/java/org/agmip/functions/PTSaxton2006.java
+++ src/main/java/org/agmip/functions/PTSaxton2006.java
@@ -1,16 +1,17 @@
 package org.agmip.functions;
 
+import java.util.HashMap;
 import static org.agmip.common.Functions.*;
 import org.agmip.common.Functions.CompareMode;
+import static org.agmip.util.MapUtil.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Implementations for the equations in the paper of 2006 PT transfer
- * calculation.
- * Method naming rule:
- * All getXXX method will be used in DOME function call by reflection.
- * All calcXXX method is used for implementing the equation from corresponding paper.
+ * calculation. Method naming rule: All getXXX method will be used in DOME
+ * function call by reflection. All calcXXX method is used for implementing the
+ * equation from corresponding paper.
  *
  * @author Meng Zhang
  */
@@ -22,75 +23,100 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
     /**
      * For calculating SLLL
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param soilParas should include 1. Sand weight percentage by layer
+     * ([0,100]%), 2. Clay weight percentage by layer ([0,100]%), 3. Organic
+     * matter weight percentage by layer ([0,100]%), (= SLOC * 1.72)
      * @return Soil water, lower limit, fraction
      */
-    public static String getSLLL(String slsnd, String slcly, String omPct) {
-        return divide(calcMoisture1500Kpa(slsnd, slcly, omPct), "100", 3);
+    public static String getSLLL(String[] soilParas) {
+        if (soilParas != null && soilParas.length >= 3) {
+            return divide(calcMoisture1500Kpa(soilParas[0], soilParas[1], soilParas[2]), "100", 3);
+        } else {
+            return null;
+        }
     }
 
     /**
      * For calculating SLDUL
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param soilParas should include 1. Sand weight percentage by layer
+     * ([0,100]%), 2. Clay weight percentage by layer ([0,100]%), 3. Organic
+     * matter weight percentage by layer ([0,100]%), (= SLOC * 1.72)
      * @return Soil water, drained upper limit, fraction
      */
-    public static String getSLDUL(String slsnd, String slcly, String omPct) {
-        return divide(calcMoisture33Kpa(slsnd, slcly, omPct), "100", 3);
-    }
-
-    /**
-     * For calculating SKSAT
-     *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
-     * @return Saturated hydraulic conductivity, cm/h
-     */
-    public static String getSKSAT(String slsnd, String slcly, String omPct) {
-        return divide(calcSatMatric(slsnd, slcly, omPct), "10", 3);
+    public static String getSLDUL(String[] soilParas) {
+        if (soilParas != null && soilParas.length >= 3) {
+            return divide(calcMoisture33Kpa(soilParas[0], soilParas[1], soilParas[2]), "100", 3);
+        } else {
+            return null;
+        }
     }
 
     /**
      * For calculating SLSAT
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param soilParas should include 1. Sand weight percentage by layer
+     * ([0,100]%), 2. Clay weight percentage by layer ([0,100]%), 3. Organic
+     * matter weight percentage by layer ([0,100]%), (= SLOC * 1.72)
      * @return Soil water, saturated, fraction
      */
-    public static String getSLSAT(String slsnd, String slcly, String omPct) {
-        return divide(calcSaturatedMoisture(slsnd, slcly, omPct), "100", 3);
+    public static String getSLSAT(String[] soilParas) {
+        if (soilParas != null && soilParas.length >= 3) {
+            return divide(calcSaturatedMoisture(soilParas[0], soilParas[1], soilParas[2]), "100", 3);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * For calculating SKSAT
+     *
+     * @param soilParas should include 1. Sand weight percentage by layer
+     * ([0,100]%), 2. Clay weight percentage by layer ([0,100]%), 3. Organic
+     * matter weight percentage by layer ([0,100]%), (= SLOC * 1.72), 4. Grave
+     * weight percentage by layer ([0,100]%)
+     * @return Saturated hydraulic conductivity, cm/h
+     */
+    public static String getSKSAT(String[] soilParas) {
+        if (soilParas != null && soilParas.length >= 3) {
+            if (soilParas.length >= 4) {
+                return divide(calcSatBulk(soilParas[0], soilParas[1], soilParas[2], divide(soilParas[3], "100")), "10", 3);
+            } else {
+                return divide(calcSatMatric(soilParas[0], soilParas[1], soilParas[2]), "10", 3);
+            }
+        } else {
+            return null;
+        }
     }
 
     /**
      * For calculating SLBDM
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param soilParas should include 1. Sand weight percentage by layer
+     * ([0,100]%), 2. Clay weight percentage by layer ([0,100]%), 3. Organic
+     * matter weight percentage by layer ([0,100]%), (= SLOC * 1.72), 4. Grave
+     * weight percentage by layer ([0,100]%)
      * @return Soil bulk density when moist for layer, g/cm3
      */
-    public static String getSLBDM(String slsnd, String slcly, String omPct) {
-        return round(calcNormalDensity(slsnd, slcly, omPct), 2);
+    public static String getSLBDM(String[] soilParas) {
+        if (soilParas != null && soilParas.length >= 3) {
+            if (soilParas.length >= 4) {
+                return round(calcGravePlusDensity(soilParas[0], soilParas[1], soilParas[2], divide(soilParas[3], "100")), 2);
+            } else {
+                return round(calcNormalDensity(soilParas[0], soilParas[1], soilParas[2]), 2);
+            }
+        } else {
+            return null;
+        }
     }
 
     /**
      * Equation 1 for calculating 1500 kPa moisture, %v
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
      */
     public static String calcMoisture1500Kpa(String slsnd, String slcly, String omPct) {
 
@@ -110,10 +136,10 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
     /**
      * Equation 2 for 33 kPa moisture, normal density, %v
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
      */
     public static String calcMoisture33Kpa(String slsnd, String slcly, String omPct) {
 
@@ -134,10 +160,10 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
     /**
      * Equation 2 for calculating 33 kPa moisture, first solution, %v
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
      */
     private static String calcMoisture33KpaFst(String slsnd, String slcly, String omPct) {
 
@@ -150,10 +176,10 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
     /**
      * Equation 3 for SAT-33 kPa moisture, normal density, %v
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
      */
     public static String calcMoistureSAT33Kpa(String slsnd, String slcly, String omPct) {
 
@@ -173,10 +199,10 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
     /**
      * Equation 5 for calculating Saturated moisture (0 kPa), normal density, %v
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
      */
     public static String calcSaturatedMoisture(String slsnd, String slcly, String omPct) {
 
@@ -191,10 +217,10 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
     /**
      * Equation 6 for calculating Normal density, g/cm-3
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
      */
     public static String calcNormalDensity(String slsnd, String slcly, String omPct) {
 
@@ -208,10 +234,10 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
     /**
      * Equation 7 for calculating Adjusted density, g/cm-3
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
      * @param df Density adjustment Factor (0.9–1.3)
      */
     public static String calcAdjustedDensity(String slsnd, String slcly, String omPct, String df) {
@@ -231,10 +257,10 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
      * Equation 8 for calculating Saturated moisture (0 kPa), adjusted density,
      * ([0,100]%)
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
      * @param df Density adjustment Factor (0.9–1.3)
      */
     public static String calcAdjustedSaturatedMoisture(String slsnd, String slcly, String omPct, String df) {
@@ -248,10 +274,10 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
     /**
      * Equation 9 for calculating 33 kPa moisture, adjusted density,([0,100]%)
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
      * @param df Density adjustment Factor (0.9–1.3)
      */
     public static String calcAdjustedMoisture33Kpa(String slsnd, String slcly, String omPct, String df) {
@@ -268,10 +294,10 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
      * Equation 10 for calculating SAT-33 kPa moisture, adjusted
      * density,([0,100]%)
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
      * @param df Density adjustment Factor (0.9–1.3)
      */
     public static String calcAdjustedMoistureSAT33Kpa(String slsnd, String slcly, String omPct, String df) {
@@ -286,10 +312,10 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
     /**
      * Equation 15 for calculating Slope of logarithmic tension-moisture curve
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
      */
     private static String calcLamda(String slsnd, String slcly, String omPct) {
 
@@ -303,10 +329,10 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
     /**
      * Equation 16 for calculating Saturated conductivity (matric soil), mm/h
      *
-     * @param slsnd Sand weight percent by layer ([0,100]%)
-     * @param slcly Clay weight percent by layer ([0,100]%)
-     * @param omPct Organic matter weight percent by layer ([0,100]%), (= SLOC *
-     * 1.72)
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
      */
     public static String calcSatMatric(String slsnd, String slcly, String omPct) {
 
@@ -315,6 +341,48 @@ public class PTSaxton2006 { //implements PTCalculatorDOMEIF {
         String lamda = calcLamda(slsnd, slcly, omPct);
         String ret = product("1930", pow(substract(satMt, mt33), substract("3", lamda)));
         LOG.debug("Calculate result for Saturated conductivity (matric soil), mm/h is {}", ret);
+        return ret;
+    }
+
+    /**
+     * Equation 20 for calculating Bulk soil density (matric plus gravel),
+     * g/cm-3
+     *
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
+     * @param slcf Grave weight fraction by layer (g/g)
+     */
+    public static String calcGravePlusDensity(String slsnd, String slcly, String omPct, String slcf) {
+
+        String slbdm = calcNormalDensity(slsnd, slcly, omPct);
+        String alpha = divide(slbdm, "2.65");
+        String slcfv = divide(product(alpha, slcf), substract("1", slcf, product("-1", slcf, alpha)));
+        String ret = sum(product(slbdm, substract("1", slcfv)), product(slcfv, "2.65"));
+        LOG.debug("Calculate result for Bulk soil density (matric plus gravel), g/cm-3 is {}", ret);
+
+        return ret;
+    }
+
+    /**
+     * Equation 22 for calculating Saturated conductivity (bulk soil), mm/h
+     *
+     * @param slsnd Sand weight percentage by layer ([0,100]%)
+     * @param slcly Clay weight percentage by layer ([0,100]%)
+     * @param omPct Organic matter weight percentage by layer ([0,100]%), (=
+     * SLOC * 1.72)
+     * @param slcf Grave weight fraction by layer (g/g)
+     */
+    public static String calcSatBulk(String slsnd, String slcly, String omPct, String slcf) {
+
+        String satMatric = calcSatMatric(slsnd, slcly, omPct);
+        String slbdm = calcNormalDensity(slsnd, slcly, omPct);
+        String alpha = divide(slbdm, "2.65");
+        String ratio = divide(substract("1", slcf), substract("1", slcf, product("-1.5", slcf, alpha)));
+        String ret = product(satMatric, ratio);
+        LOG.debug("Calculate result for Saturated conductivity (bulk soil), mm/h is {}", ret);
+
         return ret;
     }
 }
